fca48fd857f99f8c4f53ba8921d60ab79e3b250d
Emmanuel Ferdman <emmanuelferdman@gmail.com>
2025-07-05 00:42:46 +0300

    chore: integrate Prettier with ESLint configuration
    Signed-off-by: Emmanuel Ferdman <emmanuelferdman@gmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from "@eslint/js";
 
+import prettierConfig from "eslint-config-prettier";
 import jsxA11y from "eslint-plugin-jsx-a11y";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
@@ -10,7 +11,7 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, prettierConfig],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
